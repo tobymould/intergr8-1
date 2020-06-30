f1b2e0c3f8f960d32bc87cd0f7df80290103eda9
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-30 14:41:59 +0100

    remove merge conflict text

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-
-class App extends Component {
-
-  render() {
-    return(
-      <>
-      </>
-
-    )}
-}
-
-export default App;
-=======
 import React from 'react';
 import './App.css';
 import TicketCatStage1 from './TicketCatStage1';
@@ -24,4 +9,3 @@ function App() {
 }
 
 export default App;
->>>>>>> f47fbc3db3672c957cf2f2d86bc021ea3229ae40
