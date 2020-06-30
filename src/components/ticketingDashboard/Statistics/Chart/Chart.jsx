666889b77a941858ef37a075f44bc828f04e5502
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-30 16:25:41 +0100

    add chart component

---
+++ src/components/ticketingDashboard/Statistics/Chart/Chart.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Chart.module.scss';
+
+class Chart extends Component {
+  render() {
+    return (
+      <div className={styles.chart}></div>
+    );
+  }
+}
+
+export default Chart;
