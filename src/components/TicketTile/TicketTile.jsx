928a28d869d64f426c417c38b4610532f5a20ba1
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2020-06-30 14:24:05 +0100

    component folder

---
+++ src/components/TicketTile/TicketTile.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from "./TicketTile.module.scss";
+import CreateTicket from "./CreateTicket";
+
+class TicketTile extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default TicketTile;
