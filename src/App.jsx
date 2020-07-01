2a7998ec2b501c293f0ffdade8b88ea703d1e1f0
Martin Harrison <64665531+mph206@users.noreply.github.com>
2020-07-01 17:01:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-// import LogInBox from './components/LogInBox';
 import Routes from './containers/Routes/Routes';
 
 function App() {
   return (
-//     <LogInBox />
     <Routes />
   );
 }
