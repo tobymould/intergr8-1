d1e26adc3c6c88d4b77c7ea1a0c0e19f02227ca9
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-02 15:22:59 +0100

    add david styles

--- src/components/ticketingDashboard/Statistics/Chart/Chart.jsx
+++ src/components/ticketingDashboard/Statistics/Chart/Chart.jsx
@@ -6,7 +6,7 @@ class Chart extends Component {
     return (
       <svg className={styles.chart} width="200" height="200">
         <circle className={styles.outerCircle} r="60" cx="100" cy="100" />
-        <circle className={styles.innerCircle} r='45' cx="100" cy="100" />
+        <circle className={styles.innerCircle} r='55' cx="100" cy="100" />
       </svg>
     );
   }
