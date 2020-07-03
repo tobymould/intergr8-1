78eb6bc2925a5738c6e7a7abe745ec163b67a7b5
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-03 17:58:56 +0100

    move chart and add dynamic strokedash chart props

--- src/components/ticketingDashboard/ChartPanel/Chart/Chart.jsx
+++ src/components/ticketingDashboard/ChartPanel/Chart/Chart.jsx
@@ -5,7 +5,7 @@ class Chart extends Component {
   render() {
     return (
       <svg className={styles.chart} width="200" height="200">
-        <circle className={styles.outerCircle} r="60" cx="100" cy="100" />
+        <circle className={styles.outerCircle} style={{strokeDasharray: (`${this.props.strokeDash} 377`)}} r="60" cx="100" cy="100" />
         <circle className={styles.innerCircle} r='55' cx="100" cy="100" />
       </svg>
     );
