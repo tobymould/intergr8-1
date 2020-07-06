df73d69f962b313e1f78cc6ee746304fdd867068
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-06 17:10:37 +0100

    Added shadow to inner circle
    Authored with @phillipdunn

--- src/components/ticketingDashboard/ChartPanel/Chart/Chart.jsx
+++ src/components/ticketingDashboard/ChartPanel/Chart/Chart.jsx
@@ -3,11 +3,24 @@ import styles from './Chart.module.scss';
 
 class Chart extends Component {
   render() {
+    const { strokeDash } = this.props;
     return (
-      <svg className={styles.chart} width="200" height="200">
-        <circle className={styles.outerCircle} style={{strokeDasharray: (`${this.props.strokeDash} 377`)}} r="60" cx="100" cy="100" />
-        <circle className={styles.innerCircle} r='55' cx="100" cy="100" />
-      </svg>
+      <>
+        <svg className={styles.chart} width="200" height="200">
+          <circle
+            className={styles.outerCircle}
+            style={{ strokeDasharray: (`${strokeDash} 377`) }}
+            r="60" cx="100" cy="100" /> */}
+
+      <radialGradient id="myGradient">
+            <stop offset="50%" stop-color="black" />
+            <stop offset="100%" stop-color="transparent" />
+          </radialGradient>
+
+          <circle className={styles.innerShadow} r='55' cx="100" cy="107.5" fill="url(#myGradient)" />
+          <circle className={styles.innerCircle} r='55' cx="100" cy="100" />
+        </svg>
+      </>
     );
   }
 }
