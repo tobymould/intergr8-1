b61044374ef2dcd4a2f4e8a313126eb84cf08409
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2020-07-06 15:42:50 +0100

    chagnmed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Routes from './containers/Routes/Routes';
 import library from './data/fa-library';
+// import TicketView from './components/TicketView/TicketView';
 
 function App() {
 
