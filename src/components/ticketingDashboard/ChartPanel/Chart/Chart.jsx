1c7392a4e85b920a9305723bae3c1dd9359400f4
phildunn81 <phillipdunn@hotmail.com>
2020-07-06 15:25:43 +0100

    refactored chart and chart panel functions
    used object destructuring
    
    Co-authored-by: NiallJP <59969425+niallpaterson@users.noreply.github.com>

--- src/components/ticketingDashboard/ChartPanel/Chart/Chart.jsx
+++ src/components/ticketingDashboard/ChartPanel/Chart/Chart.jsx
@@ -3,9 +3,10 @@ import styles from './Chart.module.scss';
 
 class Chart extends Component {
   render() {
+    const = { strokeDash } = this.props;
     return (
       <svg className={styles.chart} width="200" height="200">
-        <circle className={styles.outerCircle} style={{strokeDasharray: (`${this.props.strokeDash} 377`)}} r="60" cx="100" cy="100" />
+        <circle className={styles.outerCircle} style={{strokeDasharray: (`${strokeDash} 377`)}} r="60" cx="100" cy="100" />
         <circle className={styles.innerCircle} r='55' cx="100" cy="100" />
       </svg>
     );
