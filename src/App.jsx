8987ef130a7e132a79f15e8091335d03e77e4d8b
Work <ej@Es-MacBook-Pro.lan>
2020-07-07 20:26:06 +0100

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Routes from "./containers/Routes/Routes";
 import "./data/fa-library";
+import CategoryChooser from "./containers/CategoryChooser";
 
 function App() {
   return <Routes />;
 }
 
 export default App;
-
