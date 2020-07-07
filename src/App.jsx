bdbc060120380a8a86fbfc9dcc4966edf773fb7f
phildunn81 <phillipdunn@hotmail.com>
2020-07-07 17:25:38 +0100

    Added NavBar to APP

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import "./App.css";
 import Routes from "./containers/Routes/Routes";
 import library from "./data/fa-library";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <Routes />;
+  return (
+  <>
+  <NavBar/>
+  <Routes />
+  </>
+  )
 }
 
 export default App;
