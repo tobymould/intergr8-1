fd97b5329bf371f9959e190daa30fb8dfdaea54d
Martin Harrison <mpharrison206@gmail.com>
2020-07-07 22:58:39 +0100

    Fix build error

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import { Router } from "@reach/router";
 
 import Login from "../../components/LogInBox";
-import Dashboard from "../../components/TicketingDashboard";
+import TicketingDashboard from "../../components/TicketingDashboard";
 import CategoryChooser from "../../components/CategoryChooser";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
-        <Dashboard path="dashboard" />
+        <TicketingDashboard path="dashboard" />
         <CategoryChooser path="ticket" />
         <Login default />
       </Router>
