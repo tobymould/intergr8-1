ae9a3f1f30ccb9c8bfb0ac7a884524499b4c758b
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-08 17:52:33 +0100

    fix conflict

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -2,17 +2,14 @@ import React, { Component } from "react";
 import { Router } from "@reach/router";
 
 import LogInBox from "../../components/LogInBox";
-import TicketingDashboard from "../../components/ticketingDashboard";
+import TicketingDashboard from "../../components/TicketingDashboard";
 import CategoryChooser from "../../components/CategoryChooser";
-
-import TicketView from "../../components/ticketingDashboard/TicketColumns/Column/TicketView";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <TicketingDashboard path="dashboard" />
-        <TicketView path="ticketview" />
         <CategoryChooser path="ticket" />
         <LogInBox default />
       </Router>
