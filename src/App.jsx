5b62327147a4425aec555952c1483eaee61db6de
Work <ej@Es-MacBook-Pro.lan>
2020-07-08 18:19:42 +0100

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Routes from "./containers/Routes/Routes.jsx";
 import "./data/fa-library";
-import CategoryChooser from "./components/CategoryChooser";
+// import CategoryChooser from "./components/CategoryChooser";
 
 function App() {
   return <Routes />;
