e7e2fbfe422a46c1a12ea576ca468506833660ba
Work <ej@Es-MacBook-Pro.lan>
2020-07-09 13:50:47 +0100

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 import Routes from "./containers/Routes/Routes.jsx";
 import "./data/fa-library";
-import CategoryChooser from "./components/CategoryChooser";
+// import CategoryChooser from "./components/CategoryChooser";
 
 function App() {
-  // return <Routes />;
-  return <CategoryChooser />;
+  return <Routes />;
+  // return <CategoryChooser />;
 }
 
 export default App;
