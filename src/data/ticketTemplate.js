0d64cbcc3ca986221934a586f2607dd94e2c831a
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-13 14:59:03 +0100

    created ticket template
    Co-authored-by: Edward Horsey <edwardhorsey@me.com>
    Co-authored-by: Rebecca Fennelly <becky.fennelly@yahoo.co.uk>
    Co-authored-by: David Wildman <59884105+TheWildebeest@users.noreply.github.com>

---
+++ src/data/ticketTemplate.js
@@ -0,0 +1,68 @@
+const ticket = [
+  {
+    createdBy: "0123456789", // Employee UID
+    assignedTo: ['Wally'], // can be multiple
+    createdAtDate: "01/01/1970",
+    modifiedAtDate: ["01/02/1970"],
+    isOpen: true,
+    priority: 3, // Time-based on date
+    eventLog: [
+      // Includes all events including messages.
+      {
+        type: "message",
+        details: "Help me!",
+        date: "01/01/1970 00:00:01",
+      },
+    ],
+  },
+];
+
+/*  Event templates
+
+Event types: 
+opened 
+message
+assignedTo 
+changeOfCategory 
+message
+isRevoled
+reOpened 
+*/
+
+{
+  type: "opened",
+  details: "Opened by Mr Open at 01/01/1970",
+  content: "Please help me!",
+  date: "01/01/1970 00:00:01",
+}
+
+{
+  type: "message",
+  details: "Help me!",
+  content: "Please help me!",
+  date: "01/01/1970 00:00:01",
+}
+
+{
+  type: "assignedTo",
+  details: "Ms. AssignedTo",
+  date: "01/01/1970 00:00:01",
+}
+
+{
+  type: "changeOfCategory",
+  details: "Help me!",
+  date: "01/01/1970 00:00:01",
+}
+
+{
+  type: "isResolved",
+  details: "Help me!",
+  date: "01/01/1970 00:00:01",
+}
+
+{
+  type: "reOpened",
+  details: "Help me!",
+  date: "01/01/1970 00:00:01",
+}
