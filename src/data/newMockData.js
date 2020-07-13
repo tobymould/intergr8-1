edd61a89c8d41673751e845da0d65a2fa20e60a0
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-07-13 15:09:09 +0100

    so blooming eccentric

--- src/data/newMockData.js
+++ src/data/newMockData.js
@@ -1,15 +1,29 @@
 const data = [
   {
     ID: "",
-    UID: "0123456789",
+    UID: "100000000",
     forename: "Niall",
     surname: "Paterson",
     email: "niall.paterson@email.com",
     password: "thisismypassword",
     img:
       "https://i0.wp.com/wordsandleaves.com/wp-content/uploads/2016/04/wally.jpg?resize=630%2C420&ssl=1",
-    role: "employee", // employee (1), agent (2), superagent (3)
+    role: 1, // employee (1), agent (2), superagent (3)
     department: null, // null for employees
     roleTextSnippet: "Person who hides",
+  },
+
+  {
+    ID: "",
+    UID: "100000001",
+    forename: "David",
+    surname: "Wildman",
+    email: "david.wildman@nology.io",
+    password: "youwillneverfindout",
+    img:
+      "https://www.placecage.com/c/200/300",
+    role: 1, // employee (1), agent (2), superagent (3)
+    department: null, // null for employees
+    roleTextSnippet: "Marketing",
   },
 ];
