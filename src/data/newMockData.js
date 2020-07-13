0ec32819a50b7293ad28f75460ab57da00f878fc
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-13 14:59:29 +0100

    added Niall Paterson to mock data
    Co-authored-by: Edward Horsey <edwardhorsey@me.com>
    Co-authored-by: Rebecca Fennelly <becky.fennelly@yahoo.co.uk>
    Co-authored-by: David Wildman <59884105+TheWildebeest@users.noreply.github.com>

---
+++ src/data/newMockData.js
@@ -0,0 +1,15 @@
+const data = [
+  {
+    ID: "",
+    UID: "0123456789",
+    forename: "Niall",
+    surname: "Paterson",
+    email: "niall.paterson@email.com",
+    password: "thisismypassword",
+    img:
+      "https://i0.wp.com/wordsandleaves.com/wp-content/uploads/2016/04/wally.jpg?resize=630%2C420&ssl=1",
+    role: "employee", // employee (1), agent (2), superagent (3)
+    department: null, // null for employees
+    roleTextSnippet: "Person who hides",
+  },
+];
