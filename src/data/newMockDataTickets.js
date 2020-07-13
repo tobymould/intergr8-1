8183bcd8c286fbe8f27570d87f03700edb7d1344
rebeccafennelly <64370782+rebeccafennelly@users.noreply.github.com>
2020-07-13 16:01:14 +0100

    Changing assignedTo and createdBy property

--- src/data/newMockDataTickets.js
+++ src/data/newMockDataTickets.js
@@ -1,8 +1,8 @@
 const ticketData = [
   {
     ID: "",
-    createdBy: 100000002, // Employee UID
-    assignedTo: ['Nick'], // can be multiple
+    createdBy: "E00000002", // Employee UID
+    assignedTo: ["S00000001"], // can be multiple
     createdAtDate: "13/07/2020",
     modifiedAtDate: [""],
     isOpen: true,
@@ -13,7 +13,7 @@ const ticketData = [
         type: "opened",
         details: "Ticket was created",
         content: {
-          name: 100000002,
+          name: "E00000002",
           message: "I haven't been paid this month."
         },
         date: "13/07/2020 13:05:43",
@@ -22,7 +22,7 @@ const ticketData = [
         type: "assignedTo",
         details: "Ticket assigned to ",
         content: {
-          name: 100000009,
+          name: "E00000009",
           message: null
         },
         date: "13/07/2020 16:05:43",
@@ -31,7 +31,7 @@ const ticketData = [
         type: "message",
         details: "New message received",
         content: {
-          name: 100000002,
+          name: "E00000002",
           message: "I still haven't heard back from HR"
         },
         date: "13/07/2020 15:05:43",
@@ -40,9 +40,57 @@ const ticketData = [
         type: "changeOfAgent",
         details: "Ticket escalated to ",
         content: {
-          name: 100000009
+          name: "E00000009"
         },
         date: "13/07/2020 17:05:43",
+      },
+    ],
+  },
+
+  {
+    ID: "",
+    createdBy: "E00000001", // Employee UID
+    assignedTo: ["S00000001"], // can be multiple
+    createdAtDate: "13/07/2020",
+    modifiedAtDate: ["14/07/2020"],
+    isOpen: true,
+    priority: 3, // Time-based on date
+    eventLog: [
+      // Includes all events including messages.
+      {
+        type: "opened",
+        details: "Ticket was created",
+        content: {
+          name: "E00000001",
+          message: "I want to findout my UTR number",
+        },
+        date: "3/07/2020 11:02:43",
+      },
+      {
+        type: "assignedTo",
+        details: "Ticket assigned to ",
+        content: {
+          name: "A00000001",
+          message: null,
+        },
+        date: "3/07/2020 16:09:43",
+      },
+      {
+        type: "message",
+        details: "New message received",
+        content: {
+          name: "E00000002",
+          message: "Hi, could you please tell me my UTR number. Thanks",
+        },
+        date: "3/07/2020 11:05:43",
+      },
+      {
+        type: "changeOfAgent",
+        details: "Ticket escalated to ",
+        content: {
+          name: "S00000001",
+        },
+        date: "14/07/2020 12:00:43",
       },
     ],
   },
