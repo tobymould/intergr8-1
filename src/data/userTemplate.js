23a76fdd55b3743e2599e93eddb4979a4b1ca6e0
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-13 14:58:52 +0100

    created user template
    Co-authored-by: Edward Horsey <edwardhorsey@me.com>
    Co-authored-by: Rebecca Fennelly <becky.fennelly@yahoo.co.uk>
    Co-authored-by: David Wildman <59884105+TheWildebeest@users.noreply.github.com>

---
+++ src/data/userTemplate.js
@@ -0,0 +1,14 @@
+const user = [
+  {
+    UID: "0123456789",
+    forename: "Wheres",
+    surname: "Wally",
+    email: "wheres.walley@privatemail.com",
+    password: "imbehindthetemple",
+    img:
+      "https://i0.wp.com/wordsandleaves.com/wp-content/uploads/2016/04/wally.jpg?resize=630%2C420&ssl=1",
+    role: "employee", // employee (1), agent (2), superagent (3)
+    department: null, // null for employees
+    roleTextSnippet: "Person who hides",
+  },
+];
