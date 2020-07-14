3c958f62368e68d10d1c18acd7a1260d783c89a8
Work <ej@Es-MacBook-Pro.lan>
2020-07-14 16:45:32 +0100

    working condition

--- src/components/TicketingDashboard/TicketColumns/Column/Column.jsx
+++ src/components/TicketingDashboard/TicketColumns/Column/Column.jsx
@@ -3,30 +3,42 @@ import styles from "./Column.module.scss";
 import DropDown from "../../../../utilities/DropDown";
 // import TicketView from "./TicketView";
 import TicketTile from "./TicketTile";
+import data from "../../../../data/mockTicketOrderData";
 
 class Column extends Component {
-  
-  
+  state = {
+    tickets: [],
+    prioritySorted: [],
+  };
+
+  componentDidMount() {
+    //get from firebase
+    // this.setState({ tickets: data, prioritySorted: filterClicked(data)})
+  }
+
   renderFilter() {
     return this.props.filter ? (
-      <DropDown
-        filterOptions={["Date: earliest", "Date: latest", "Category"]}
-      />
+      <DropDown filterOptions={["Newest", "Oldest", "Priority", "Category"]} />
     ) : (
       <p>{}</p>
     );
   }
 
   filterClicked = (e) => {
-    if (e.target.value ==="Date: earliest") {
-      //this.data.sort()
-      console.log('clicked1');
-    } else if (e.target.value ==="Date: latest"){
-      console.log('clicked2');
-    } else {
-      console.log('clicked3')
+    if (e.target.value === "Newest") {
+      let newestDates = data.sort((a, b) => new Date(b.createdAtDate) - new Date(a.createdAtDate));
+      console.log(newestDates);
+    } else if (e.target.value === "Oldest") {
+      let oldestDates = data.sort((a, b) => new Date(a.createdAtDate) - new Date(b.createdAtDate));
+      console.log(oldestDates);
+    } else if (e.target.value === "Priority") {
+      const prioritySort = data.sort((a, b) => b.priority - a.priority);
+      console.log(prioritySort);
+    } else if (e.target.value === "Category ") {
+      const categorySort = data.sort((a, b) => b.category - a.category);
+      console.log(categorySort);
     }
-}
+  };
 
   render() {
     return (
@@ -37,14 +49,7 @@ class Column extends Component {
             <div onChange={this.filterClicked}>{this.renderFilter()}</div>
           </div>
           <section title={this.props.title}>
-            <TicketTile />
-            <TicketTile />
-            <TicketTile />
-            <TicketTile />
-            <TicketTile />
-            <TicketTile />
-            <TicketTile />
-            <TicketTile />
+            <TicketTile/>
           </section>
         </article>
         {/* <TicketView /> */}
