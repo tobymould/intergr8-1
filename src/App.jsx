ff936c67b32099b552224fe1e7a8239c1b7a8951
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-07-14 16:24:45 +0100

    added login method to app class

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import React, { Component } from "react";
 import "./App.css";
 import Routes from "./containers/Routes/Routes.jsx";
 import "./data/fa-library";
+import firebase from "./firebase";
 
 class App extends Component {
 
+  state = {
+    user: null,
+    emailAddress: null,
+    password: null
+  };
+
+  userSignInAttempt = () => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+      })
+  }
+
+
   render() {
-    return <Routes />
+    return <Routes userSignInAttempt={this.userSignInAttempt} user={this.user} emailAddress={this.emailAddress} password={this.password} />
   }
 }
 
