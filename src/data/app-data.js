dc3d41d7c4028fadacebbd739ad7451c82c2fc85
tobymould <toby.mould@icloud.com>
2020-07-14 15:45:50 +0100

    added provider authentication

---
+++ src/data/app-data.js
@@ -0,0 +1,68 @@
+export const people = [
+  {
+    id: "1",
+    firstName: "Toby",
+    lastName: "Mould",
+    image: "https://galapagos.nology.io/images/trainees/Toby.jpeg",
+  },
+  {
+    id: "2",
+    firstName: "Stephanie",
+    lastName: "Hansen",
+    image: "https://galapagos.nology.io/images/trainees/Steph.jpg",
+  },
+  {
+    id: "3",
+    firstName: "Martin",
+    lastName: "Harrison",
+    image: "https://galapagos.nology.io/images/trainees/martin.jpg",
+  },
+  {
+    id: "4",
+    firstName: "Rebecca",
+    lastName: "Fennelly",
+    image: "https://galapagos.nology.io/images/trainees/rebecca.jpg",
+  },
+  {
+    id: "5",
+    firstName: "Edward",
+    lastName: "Horsey",
+    image: "https://galapagos.nology.io/images/trainees/edward.png",
+  },
+  {
+    id: "6",
+    firstName: "EJ",
+    lastName: "Ichiebuka",
+    image: "https://galapagos.nology.io/images/trainees/Emmanuel.jpg",
+  },
+  {
+    id: "7",
+    firstName: "Philip",
+    lastName: "Dunn",
+    image: "https://galapagos.nology.io/images/trainees/phil.jpg",
+  },
+    {
+    id: "8",
+    firstName: "Niall",
+    lastName: "Paterson",
+    image: "https://galapagos.nology.io/images/trainees/niall.jpg",
+  },
+  {
+    id: "9",
+    firstName: "David",
+    lastName: "Wildman",
+    image: "https://galapagos.nology.io/images/trainees/david.jpg",
+  },
+  {
+    id: "10",
+    firstName: "Sumit",
+    lastName: "Gurung",
+    image: "https://galapagos.nology.io/images/trainees/sumit.png",
+  },
+];
+
+//name
+//image
+//id
+//user status
+//department
