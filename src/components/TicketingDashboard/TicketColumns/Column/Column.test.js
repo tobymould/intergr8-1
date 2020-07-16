0b0ae2cb5b7c7789a82a668c96eec22148131280
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2020-07-16 17:05:04 +0100

    even more conflicts

--- src/components/TicketingDashboard/TicketColumns/Column/Column.test.js
+++ src/components/TicketingDashboard/TicketColumns/Column/Column.test.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Column from './Column';
-import { shallow } from "enzyme";
-
-
-describe("Column component tests", () => {
-
-  let testComponent;
-
-  beforeEach(() => {
-    testComponent = shallow(<Column />);
-  })
-
-  it("should render", () => {
-    expect(testComponent).toBeTruthy();
-  })
-
-  
-})
-=======
 import React from "react";
 import { shallow } from "enzyme";
 import Column from "./Column";
@@ -30,4 +9,3 @@ describe("Stage1 component tests", () => {
     expect(component).toBeTruthy();
   });
 });
->>>>>>> 6a297b302b4198b349bcf8ccd31e9128886e58a0
