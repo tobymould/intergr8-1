99f99015afc0212f639a53020616030773843493
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-07-17 20:30:59 +0100

    private routes logic

--- src/containers/PrivateRoutes.jsx
+++ src/containers/PrivateRoutes.jsx
@@ -3,6 +3,7 @@ import firebase from "../firebase";
 import { Router, globalHistory } from "@reach/router";
 import TicketingDashboard from '../components/TicketingDashboard';
 import CategoryChooser from '../components/CategoryChooser';
+import SuperUserDashboard from '../components/SuperUserDashboard';
 import CreateTicket from '../components/CategoryChooser/CreateTicket';
 import TicketView from '../components/TicketingDashboard/TicketColumns/Column/TicketView';
 
@@ -31,6 +32,7 @@ class PrivateRoutes extends Component {
         <CategoryChooser path="ticket" user={user} signOut={signOut} />
         <TicketView path="viewticket" user={user} signOut={signOut} />
         <CreateTicket path="createticket" user={user} signOut={signOut} />
+        <SuperUserDashboard path="superuser" />
       </Router>
     )
       ;
