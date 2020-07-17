c75a2d68fedace4557bc90e56e759df798324c73
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-07-17 20:25:38 +0100

    adding firebase services back in

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,19 @@
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/storage';
-import 'firebase/auth';
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAMW3ReoJUdI2XjjksGewsKQIpM1pE4MmA',
-  authDomain: 'intergr8-42df9.firebaseapp.com',
-  databaseURL: 'https://intergr8-42df9.firebaseio.com',
-  projectId: 'intergr8-42df9',
-  storageBucket: 'intergr8-42df9.appspot.com',
-  messagingSenderId: '648450503153',
-  appId: '1:648450503153:web:680b25c0f4ee7ceef29450',
-  measurementId: 'G-L2BT02L4KP'
+  apiKey: "AIzaSyAMW3ReoJUdI2XjjksGewsKQIpM1pE4MmA",
+  authDomain: "intergr8-42df9.firebaseapp.com",
+  databaseURL: "https://intergr8-42df9.firebaseio.com",
+  projectId: "intergr8-42df9",
+  storageBucket: "intergr8-42df9.appspot.com",
+  messagingSenderId: "648450503153",
+  appId: "1:648450503153:web:680b25c0f4ee7ceef29450",
+  measurementId: "G-L2BT02L4KP"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
