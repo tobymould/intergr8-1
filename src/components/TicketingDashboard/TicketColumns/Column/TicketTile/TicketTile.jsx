44179259826d74358d40ce577eadfc7bd25aaf50
tobymould <toby.mould@icloud.com>
2020-07-21 16:07:55 +0100

    created fetch request to get users from firebase inside ticket tile, and added in image of user
    Co-authored-by: Edward Horsey <edward.horsey@nology.io>
    '

--- src/components/TicketingDashboard/TicketColumns/Column/TicketTile/TicketTile.jsx
+++ src/components/TicketingDashboard/TicketColumns/Column/TicketTile/TicketTile.jsx
@@ -1,13 +1,43 @@
-import React, { Component } from "react";
-import styles from "./Tickettile.module.scss";
+import React, { Component } from 'react';
+import styles from './Tickettile.module.scss';
+import { firestore } from '../../../../../firebase';
 
 class TicketTile extends Component {
+  state = { users: [] };
+
+  // fetchImage = PersonID => {
+  //   return user;
+  // };
+
+  getUsers = () => {
+    firestore
+      .collection('info')
+      .get()
+      .then(snapshot => {
+        const users = snapshot.docs.map(doc => doc.data());
+        this.setState({ users });
+      });
+  };
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  /* 
+    obj is a ticket
+    obj.assigned, []
+
+    fetch firebase for 
+  
+  */
   render() {
+    console.log(this.state);
+
     return (
       <div>
-        {this.props.data.map((obj) => (
+        {this.props.data.map(obj => (
           <section className={styles.TicketTile}>
-            <div className={styles.TicketContent} >
+            <div className={styles.TicketContent}>
               <h3>{obj.title}</h3>
               <p>Employee ID: {obj.createdBy}</p>
               <p>Ticket ID: {obj.ID}</p>
@@ -15,10 +45,7 @@ class TicketTile extends Component {
             </div>
             <div>{/* <input type="checkbox"/> */}</div>
             <div>
-              <select name="" id="">
-                <option value="Agent1">Agent1</option>
-                <option value="Agent2">Agent2</option>
-              </select>
+              <img src={this.state.users[0].image} alt={this.state.users[0].name} />
             </div>
           </section>
         ))}
