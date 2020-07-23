45cf85cb5ae50e29e389ae675a697ea37e164a56
Martin Harrison <mpharrison206@gmail.com>
2020-07-23 21:49:24 +0100

    Fix test

--- src/components/UserDashboard/UserDashboard.test.js
+++ src/components/UserDashboard/UserDashboard.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { shallow, ReactWrapper } from "enzyme";
-import TicketingDashboard from "./TicketingDashboard";
+import UserDashboard from "./UserDashboard";
 
 describe("Stage1 component tests", () => {
-  const component = shallow(<TicketingDashboard />, { disableLifecycleMethods: true });
+  const component = shallow(<UserDashboard />, { disableLifecycleMethods: true });
 
   it("should render", () => {
     expect(component).toBeTruthy();
