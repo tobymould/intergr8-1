0de124a1ba4a698cd554ce211addb583b5fb0a5f
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-23 17:35:10 +0100

    fix test

--- src/components/SuperUserDashboard/EditCategory/EditCategory.test.js
+++ src/components/SuperUserDashboard/EditCategory/EditCategory.test.js
@@ -3,7 +3,14 @@ import { shallow } from "enzyme";
 import EditCategory from "./EditCategory";
 
 describe("Stage1 component tests", () => {
-  const component = shallow(<EditCategory />, { disableLifecycleMethods: true });
+  const component = shallow(<EditCategory 
+    setCategoriesState={()=> {console.log('function')}}
+    addSubcategory={()=> {console.log('function')}}
+    removeSubcategory={()=> {console.log('function')}}
+    categories={[{
+      queries: ['test'],
+      title: 'test'
+    }]} />, { disableLifecycleMethods: true });
 
   it("should render", () => {
     expect(component).toBeTruthy();
