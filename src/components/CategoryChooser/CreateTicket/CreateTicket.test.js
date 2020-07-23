3b6d358816ed12b5df0284b28d2ce947cc932708
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-07-23 03:35:00 +0100

    test is failing, adding some data to the ticket

--- src/components/CategoryChooser/CreateTicket/CreateTicket.test.js
+++ src/components/CategoryChooser/CreateTicket/CreateTicket.test.js
@@ -1,13 +1,14 @@
 import React from "react";
 import CreateTicket from "./CreateTicket";
 import { shallow } from 'enzyme';
+import sampleTickets from '../../../data/mockTicketOrderData';
 
 describe("CreateTicket tests", () => {
-  
+
   let component;
 
   beforeEach(() => {
-    component = shallow(<CreateTicket user={{uid:123}} choices={["Health & Safety", "What health and safety policies are in place"]}/>);
+    component = shallow(<CreateTicket data={sampleTicket[4]} choices={["Health & Safety", "What health and safety policies are in place"]} />);
   })
 
   it('should render', () => {
