a939cc9a45785610e8fee766b837978db01081c6
Work <ej@Es-MacBook-Pro.lan>
2020-07-23 10:04:09 +0100

    fixed message

--- src/components/TicketingDashboard/TicketColumns/Column/TicketView/Message/Message.jsx
+++ src/components/TicketingDashboard/TicketColumns/Column/TicketView/Message/Message.jsx
@@ -19,26 +19,24 @@ class Message extends Component {
     const message = (
       <div className={styles.messageContent}>
         <div className={styles.nameAndDate}>
-          <h3>{"this.name"}</h3>
+          <h3>Employee Name</h3>
           <p>Date and time</p>
         </div>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nam maxime
-          sit. At est magnam voluptates, minus iusto sit inventore nisi illum
-          laudantium sed in ullam, vitae rerum beatae officia?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nam maxime sit. At est magnam
+          voluptates, minus iusto sit inventore nisi illum laudantium sed in ullam, vitae rerum
+          beatae officia?
         </p>
       </div>
     );
 
     if (this.props.userType === "Employee") {
-      name = "Employee";
       return (
         <>
           {pic} {message}
         </>
       );
     } else {
-      name = "HR Agent";
       return (
         <>
           {message} {pic}
