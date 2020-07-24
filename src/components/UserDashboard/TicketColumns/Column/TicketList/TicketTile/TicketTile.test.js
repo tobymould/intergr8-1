e30ddec2090613f0b21b582363329353df050b7a
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-07-24 11:33:20 +0100

    tweaking test

--- src/components/UserDashboard/TicketColumns/Column/TicketList/TicketTile/TicketTile.test.js
+++ src/components/UserDashboard/TicketColumns/Column/TicketList/TicketTile/TicketTile.test.js
@@ -6,31 +6,28 @@ describe("ticket tile component tests", () => {
   let testComponent;
 
   beforeEach(() => {
-    testComponent = shallow(<TicketTile
-      key="woiwrjoijwvojrwvw"
-      ticket={{
-        ID: "WijGFghjBVftYHBVFthbvFTY",
-        isOpen: true,
-        name: "Seth McFarlane",
-        createdBy: "Boaty McBoatface",
-        eventLog=
-        [
-          { content: { message: "HELLO" } },
-          { content: { message: "WHYHELLO!" } },
-          { content: { message: "HOWRU?" } }
-        ]
-      }}
-
-      currentTicket={{
-        ID: "WijGFghjBVftYHBVFthbvFTY",
-        isOpen: true, name:
-          "Seth McFarlane",
-        createdBy: "Boaty McBoatface"
-      }}
-
-      setCurrentTicket={console.log("hello")}
-
-    />)
+    testComponent = shallow(
+      <TicketTile
+        key="woiwrjoijwvojrwvw"
+        ticket={{
+          ID: "WijGFghjBVftYHBVFthbvFTY",
+          eventLog={
+            0: [
+              { content: { message: "HELLO" } }
+            ]
+          },
+          isOpen: true,
+          name: "Seth McFarlane",
+          createdBy: "Boaty McBoatface"
+        }}
+        currentTicket={{
+          ID: "WijGFghjBVftYHBVFthbvFTY",
+          isOpen: true, name:
+            "Seth McFarlane",
+          createdBy: "Boaty McBoatface"
+        }}
+        setCurrentTicket={console.log("hello")}
+      />)
   });
 
   it("should render", () => {
